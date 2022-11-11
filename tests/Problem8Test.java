6cb5a5a7314ce97cc060b63049a024f47045ee71
ChanochW <cwinogra@student.touro.edu>
2022-11-10 23:19:30 -0500

    Problem 8 passes.

---
+++ tests/Problem8Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem8Test {
+    @Test
+    public void project8Test() {
+        long expected = 23514624000L;
+        assertEquals(expected, Problem8.main());
+    }
+}
